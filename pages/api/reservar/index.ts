6899537f898725d2b42109cd371cae7ce4dbd0cd
unaicanales <36845145+Seretoiden@users.noreply.github.com>
2023-02-24 13:01:20 +0100

    Añadido control de acceso mediante los query params del GET :

--- pages/api/reservar/index.ts
+++ pages/api/reservar/index.ts
@@ -3,34 +3,38 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { conn } from "../../../utils/database";
 
 type Data = {
-  name: string;
+  message: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { method, body } = req;
-  let query = "";
+  const { method, body, headers, query } = req;
+  let querie = "";
 
   console.log(method);
   console.log(body);
 
   switch (method) {
     case "GET":
-      query = "SELECT * FROM public.reserva ORDER BY id ASC;";
-      try {
-        let getResponse = await conn.query(query);
+      if (query.key !== undefined && query.key === "holaquetalestamos") {
+        querie = "SELECT * FROM public.reserva ORDER BY id ASC;";
+        try {
+          let getResponse = await conn.query(querie);
 
-        return res.status(200).json(getResponse.rows);
-      } catch (error: any) {
-        return res.status(400).json(error.message);
+          return res.status(200).json(getResponse.rows);
+        } catch (error: any) {
+          return res.status(400).json(error.message);
+        }
+      } else {
+        return res.status(400).json({ message: "method not implemented" });
       }
 
     case "POST":
       const { nombre, personas, dia, hora, telefono, email } = body;
 
-      query =
+      querie =
         "INSERT INTO public.reserva(nombre, personas, dia, hora, telefono, email) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;";
       const values = [nombre, personas, dia, hora, telefono, email];
 
@@ -43,9 +47,9 @@ export default async function handler(
       }
 
     case "DELETE":
-      return res.status(200).json({ name: "DELETE Reserva" });
+      return res.status(200).json({ message: "DELETE Reserva" });
 
     default:
-      return res.status(400).json({ name: "Method not allowed" });
+      return res.status(400).json({ message: "Method not allowed" });
   }
 }
